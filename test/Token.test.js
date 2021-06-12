bf7c156df6b64e27a2b821b067b3c6a9382b5c05
xTeKc <bcd3v@pm.me>
2021-06-12 10:03:56 -0400

    update token test

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,5 +1,9 @@
 const Token = artifacts.require('./Token')
 
+require('chai')
+	.use(require('chai-as-promised'))
+	.should()
+
 contract('Token', (accounts) => {
 
 	descrbibe('deployment', () => {
