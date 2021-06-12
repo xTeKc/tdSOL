34342b0cbe9b205fb4d26893ab684754cb0f7f71
xTeKc <bcd3v@pm.me>
2021-06-12 13:27:44 -0400

    update token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -3,21 +3,21 @@ pragma solidity ^0.5.0;
 
 contract Token {
 
-string public name = 'Digital Gold';
-string public symbol = 'DGLD';
-uint256 public decimals = 18;
-uint256 public totalSupply;
+	string public name = 'Digital Gold';
+	string public symbol = 'DGLD';
+	uint256 public decimals = 18;
+	uint256 public totalSupply;
 
-//Track balances
-mapping (address => uint256) public balanceOf;
+	//Track balances
+	mapping (address => uint256) public balanceOf;
 
-//Send tokens
+	//Send tokens
 
 
-constructor() public {
-	totalSupply = 1000000 * (10 ** decimals);
-	balanceOf[msg.sender] = totalSupply;
-}
+	constructor() public {
+		totalSupply = 1000000 * (10 ** decimals);
+		balanceOf[msg.sender] = totalSupply;
+	}
 
 }
 
