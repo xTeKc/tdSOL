ce3d83d5b46b3f3a0f7ed33debd2cf99c3fbb102
xTeKc <bcd3v@pm.me>
2021-06-12 10:19:33 -0400

    update token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.5.0;
 
 contract Token {
 
-string public name = "Digital Gold";
+string public name = 'Digital Gold';
+string public symbol = 'DGLD';
+uint256 public decimals = 18;
+uint256 public totalSupply = 1000000;
 
 }
 
