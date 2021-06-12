2884d96caf0ae340faf77ed3e1d6268c1913ac40
xTeKc <bcd3v@pm.me>
2021-06-12 15:15:55 -0400

    update token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -17,6 +17,8 @@ contract Token {
 
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+		balanceOf[_to] = balanceOf[_to].add(_value);
+		return true;
 	}
 
 }
