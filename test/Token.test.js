0bf661878c17ab20a8858ca07332c0367520a454
xTeKc <bcd3v@pm.me>
2021-06-12 10:02:00 -0400

    update token test

--- test/Token.test.js
+++ test/Token.test.js
@@ -3,7 +3,7 @@ const Token = artifacts.require('./Token')
 contract('Token', (accounts) => {
 
 	descrbibe('deployment', () => {
-		it('tracks the name', () => {
+		it('tracks the name', async () => {
 			token = await Token.new()
 			//Read token name here.. 
 			//Check the token name is 'Digital Gold'
