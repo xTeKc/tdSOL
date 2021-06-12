383aded1f075e8e88d940be5dfcdf1338b643bfc
xTeKc <bcd3v@pm.me>
2021-06-12 10:00:30 -0400

    update token test

--- test/Token.test.js
+++ test/Token.test.js
@@ -2,6 +2,12 @@ const Token = artifacts.require('./Token')
 
 contract('Token', (accounts) => {
 
-	
-	
+	descrbibe('deployment', () => {
+		it('tracks the name', () => {
+			//Fetch token from blockchain
+			//Read token name here.. 
+			//Check the token name is 'Digital Gold'
+		})
+	})
+
 })
