90705b943a732fe7fd063498e466e7a4097a3cdf
xTeKc <bcd3v@pm.me>
2021-06-14 10:54:41 -0400

    add new transferFrom test

--- test/Token.test.js
+++ test/Token.test.js
@@ -128,5 +128,57 @@ contract('Token', ([deployer,receiver, exchange]) => {
 
 	})
 
+	describe('sending tokens', () => {
+		let amount
+		let result
+
+		beforeEach(async () => {
+			amount = tokens(100)
+			await token.approve(exchange, amount, { from: deployer })
+		})
+
+	describe('success', async () => {
+		beforeEach(async () => {
+			result = await token.transferFrom(deployer, receiver, amount, { from: exchange })
+		})
+
+		it('transfers token balances', async () => {
+			let balanceOf
+
+			balanceOf = await token.balanceOf(deployer)
+			balanceOf.toString().should.equal(tokens(999900).toString())
+			balanceOf = await token.balanceOf(receiver)
+			balanceOf.toString().should.equal(tokens(100).toString())
+		})
+
+		// it('emits Transfer event', async () => {
+		// 	const log = result.logs[0]
+		// 	log.event.should.equal('Transfer')
+		// 	const event = log.args
+		// 	event.from.toString().should.equal(deployer, 'from value is correct')
+		// 	event.to.should.equal(receiver, 'to is correct')
+		// 	event.value.toString().should.equal(amount.toString(), 'value is correct')
+		// })		
+	})
+
+	describe('failure', async () => {
+
+		// it('rejects insufficient balances', async () => {
+		// 	let invalidAmount
+		// 	invalidAmount = tokens(100000000) //100 mil (greater than total supply - invalid)
+		// 	await token.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(EVM_REVERT)
+
+		// 	invalidAmount = tokens(10) //recipient has no tokens
+		// 	await token.transfer(deployer, invalidAmount, { from: receiver }).should.be.rejectedWith(EVM_REVERT)
+		// })
+	
+		// it('rejects invalid recipients', async () => {
+		// 	await token.transfer(0x0, amount, { from: deployer }).should.be.rejected
+		// })
+
+	})	
+
+	})	
+
 })
 
