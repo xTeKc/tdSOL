9886d383e076c44f4e3d8221ef8406adc51ff1b8
xTeKc <bcd3v@pm.me>
2021-06-14 10:15:45 -0400

    emit approval event

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -32,6 +32,7 @@ contract Token {
 
 	function approve(address _spender, uint256 _value) public returns (bool success) {
 		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 
