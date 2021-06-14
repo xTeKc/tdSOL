9654093f2246829a2719eaf35a3741665fc891c2
xTeKc <bcd3v@pm.me>
2021-06-14 11:07:28 -0400

    add allowance to transferFrom func

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -42,6 +42,7 @@ contract Token {
 	}
 
 	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
 		_transfer(_from, _to, _value);
 		return true;
 	}
