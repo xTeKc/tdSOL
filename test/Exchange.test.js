a4378ba416abded60c76221023bac458586c913f
xTeKc <bcd3v@pm.me>
2021-06-15 23:12:06 -0400

    update exchange test

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -14,30 +14,7 @@ contract('Exchange', ([deployer, feeAccount]) => {
 	})
 
 	describe('deployment', () => {
-		it('tracks the name', async () => {
-			const result = await token.name()
-			result.should.equal(name)
-		})
 
-		it('tracks the symbol', async () => {
-			const result = await token.symbol()
-			result.should.equal(symbol)
-		})
-
-		it('tracks the decimals', async () => {
-			const result = await token.decimals()
-			result.toString().should.equal(decimals)			
-		})
-
-		it('tracks the total supply', async () => {
-			const result = await token.totalSupply()
-			result.toString().should.equal(totalSupply.toString())			
-		})
-
-		it('assigns the total supply to the deployer', async () => {
-			const result = await token.balanceOf(deployer)
-			result.toString().should.equal(totalSupply.toString())
-		})
 
 
 	})
