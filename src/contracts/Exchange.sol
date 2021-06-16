588531823ae645835ad04dab81e17d1069477718
xTeKc <bcd3v@pm.me>
2021-06-15 23:02:27 -0400

    update exchange

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,3 +1,15 @@
 // Deposit & Withdraw Funds
 // Manage Orders - Make or Cancel
 // Handle Trades - Charge Fees
+
+// TODO
+//Set the fee account
+//Deposit Ether
+//Withdraw Ether
+//Deposit Tokens
+//Withdraw Tokens
+//Check balances
+//Make order
+//Cancel order
+//Fill order
+//Charge fees
