8cbcc3b4bd118821b0ffec801ff2c6056ac22469
xTeKc <bcd3v@pm.me>
2021-06-16 17:30:23 -0400

    make test for making orders

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -215,5 +215,18 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 
     })
 
+    describe('making orders', async () => {
+        let result
+
+        beforeEach(async () => {
+            result = await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), { from: user1 })
+        })
+
+        it('tracks the newly created order', async () => {
+            const orderCount = await exchange.orderCount()
+            orderCount.toString().should.equal('1')
+        })
+    })
+
  })
 
