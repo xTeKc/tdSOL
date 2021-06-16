517dba005387ea722f39d82aa5be6bcc96d0df75
xTeKc <bcd3v@pm.me>
2021-06-16 14:37:19 -0400

    emit withdrawToken event

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -66,6 +66,7 @@ contract Exchange {
     function withdrawToken(address _token, uint256 _amount) public {
         tokens[_token][msg.sender] = tokens[_token][msg.sender].sub(_amount);
         require(Token(_token).transfer(msg.sender, _amount));
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
 }
