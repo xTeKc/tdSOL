0fe83d5ffcbc6fef30f4df039f7e73fb9f3599ca
xTeKc <bcd3v@pm.me>
2021-06-16 06:59:24 -0400

    update exchange contract

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,5 +1,15 @@
 pragma solidity ^0.5.0;
 
+contract Exchange {
+
+    address public feeAccount; //fee account that receives exchange fees
+
+    constructor(address _feeAccount) public {
+        feeAccount = _feeAccount;
+    }
+
+}
+
 
 // Deposit & Withdraw Funds
 // Manage Orders - Make or Cancel
