367d4071930bd0dd50c2cf30c6df3da53b18ef57
xTeKc <bcd3v@pm.me>
2021-06-16 14:55:23 -0400

    create test for balanceOf

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -203,5 +203,17 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 
     })
 
+    describe('checking balances', async () => {
+        beforeEach(async () => {
+            exchange.depositEther({ from: user1, value: ether(1) })
+        })
+
+        it('returns user balance', async () => {
+            const result = await exchange.balanceOf(ETHER_ADDRESS, user1)
+            result.toString().should.equal(ether(1).toString())
+        })
+
+    })
+
  })
 
