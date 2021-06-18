1427bfae8660fc7e322cc4fa5b3c0f41d8f3fe03
xTeKc <bcd3v@pm.me>
2021-06-17 22:19:28 -0400

    update deploy file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,4 +3,5 @@ const Exchange = artifacts.require("Exchange");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Token);
+  await deployer.deploy(Exchange, 'fee account', 10);
 };
