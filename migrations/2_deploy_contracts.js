2217ec1e78792fc3b7f39c3ac0dd3089d65911a7
xTeKc <bcd3v@pm.me>
2021-06-17 22:20:29 -0400

    update deploy file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
+const { default: Web3 } = require("web3");
+
 const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
 module.exports = async function (deployer) {
+  const accounts = await Web3.eth.getAccounts()
+
   await deployer.deploy(Token);
   await deployer.deploy(Exchange, 'fee account', 10);
 };
