82eb16d33379b01ab96e2789db146358d4af9547
xTeKc <bcd3v@pm.me>
2021-06-19 08:35:14 -0400

    update script

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -14,6 +14,14 @@ module.exports = async function(callback) {
         const exchange = await Exchange.deployed()
         console.log('Exchange fetched', exchange.address)
 
+        //Give Tokens to account[1]
+        const sender = accounts[0]
+        const receiver = accounts[1]
+        let amount = web3.utils.toWei('10000', 'ether') // 10_000 tokens
+
+        await token.transfer(receiver, amount, { from: sender })
+        console.log('Transferred ${amount} tokens from ${sender} to ${receiver}')
+
     } 
     catch(err) {
         console.log(error)
