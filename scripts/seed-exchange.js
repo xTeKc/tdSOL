d6d60c05ce869972a44ed5954f1ed062d9b023f3
xTeKc <bcd3v@pm.me>
2021-06-19 08:31:06 -0400

    update script

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -1,6 +1,15 @@
+const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
+
 module.exports = async function(callback) {
     try {
-        console.log('script running...')
+        //Fetch accounts from wallet (they are unlocked)
+        const accounts = await web3.eth.getAccounts()
+
+        //Fetch the deployed Token
+        const token = await Token.deployed()
+        console.log('Token fetched', token.address)
+
     } 
     catch(err) {
         console.log(error)
