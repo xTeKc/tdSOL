57d0460c974c7f7f1cdd0619e0ed66b15b243b6c
xTeKc <bcd3v@pm.me>
2021-06-21 22:15:07 -0400

    fix unreachable code

--- truffle-config.js
+++ truffle-config.js
@@ -17,7 +17,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.0",
+      version: "0.8.0",
       optimizer: {
         enabled: true,
         runs: 200
