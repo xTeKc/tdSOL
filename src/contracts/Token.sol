ea425234e2732b8502ce87eb472f950799be1cda
xTeKc <bcd3v@pm.me>
2021-06-21 22:36:29 -0400

    refactor

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -15,8 +15,8 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
-	//define msg.sender of contract
-	//address payable public msg.sender;
+	//define owner of contract
+	//address payable public owner;
 
 	//mapping an address to an amount
 	mapping(address => uint256) public balanceOf;
@@ -28,26 +28,26 @@ contract Token {
 	//event for the approval of amount
 	event Approval(address indexed owner, address indexed spender, uint256 value);
 
-	//setting totalSupply and msg.sender, with the msg.sender = to the totalSupply
+	//setting totalSupply and owner, with the owner = to the totalSupply
 	constructor() {
 		totalSupply = 1000000 * (10 ** decimals);
-		balanceOf[msg.sender] = totalSupply;
-		//make msg.sender be equal to msg.sender
-		//msg.sender == msg.sender;
+		balanceOf[owner] = totalSupply;
+		//make owner be equal to owner
+		msg.sender == owner;
 	}
 
 	//transfer func with local variables of _to and _value
 	//making transfer from supply and to the address _to with a num _value of, return a bool
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		//req balance of the supply to be greater than or equal to the _value
-		require(balanceOf[msg.sender] >= _value);
-		_transfer(msg.sender, _to, _value);
+		require(balanceOf[owner] >= _value);
+		_transfer(owner, _to, _value);
 		return true;
 	}
 
     //private _transfer func with local variables of _from address, _to address and _value 
 	function _transfer(address _from, address _to, uint256 _value) internal {
-		//req the _to address to Not be the msg.sender(address[0])
+		//req the _to address to Not be the owner(address[0])
 		require(_to != address(0));
 		balanceOf[_from] = balanceOf[_from].sub(_value);
 		balanceOf[_to] = balanceOf[_to].add(_value);
@@ -55,17 +55,17 @@ contract Token {
 	}
 
 	function approve(address _spender, uint256 _value) public returns (bool success) {
-		//req _spender to not be the msg.sender(address[0])
+		//req _spender to not be the owner(address[0])
 		require(_spender != address(0));
-		allowance[msg.sender][_spender] = _value;
-		emit Approval(msg.sender, _spender, _value);
+		allowance[owner][_spender] = _value;
+		emit Approval(owner, _spender, _value);
 		return true;
 	}
 
 	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
 		require(_value <= balanceOf[_from]);
-		require(_value <= allowance[_from][msg.sender]);
-		allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+		require(_value <= allowance[_from][owner]);
+		allowance[_from][owner] = allowance[_from][owner].sub(_value);
 		_transfer(_from, _to, _value);
 		return true;
 	}
