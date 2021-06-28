91388520f1931bd0e5776f11fe55bad5ef8b731b
xTeKc <bcd3v@pm.me>
2021-06-28 12:31:10 -0400

    tokenloaded

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -3,3 +3,6 @@ import { createSelector } from 'reselect'
 
 const account = state => get(state, 'web3.account')
 export const accountSelector = createSelector(account, a => a)
+
+const tokenLoaded = state => get(state, 'token.loaded', false)
+export const tokenLoadedSelector = createSelector(tokenLoaded, tl => tl)
