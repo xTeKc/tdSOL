fe38664a40d008b9849693de12394c5b687f7351
xTeKc <bcd3v@pm.me>
2021-06-28 14:21:03 -0400

    import trades

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { exchangeSelector } from '../store/selectors'
 import { loadAllOrders } from '../store/interactions'
+import Trades from './Trades'
 
 class Content extends Component {
   componentWillMount() {
