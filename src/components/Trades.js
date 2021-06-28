79674e4a05aeffcb1d13aa6462b71c8099b953d3
xTeKc <bcd3v@pm.me>
2021-06-28 14:49:11 -0400

    update

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -35,8 +35,8 @@ class Trades extends Component {
               <thead>
                 <tr>
                   <th>Time</th>
-                  <th>DAPP</th>
-                  <th>DAPP/ETH</th>
+                  <th>DGLD</th>
+                  <th>DGLD/ETH</th>
                 </tr>
               </thead>
               { this.props.filledOrdersLoaded ? showFilledOrders(this.props.filledOrders) : <Spinner type="table" />}
