41b5dfb65f9edf34c478e42089d0e8728cf8bd58
xTeKc <bcd3v@pm.me>
2021-06-28 17:19:05 -0400

    refactor

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,51 +1,65 @@
 // WEB3
 export function web3Loaded(connection) {
-    return {
-      type: 'WEB3_LOADED',
-      connection
-    }
+  return {
+    type: 'WEB3_LOADED',
+    connection
   }
+}
 
-  export function web3AccountLoaded(account) {
-    return {
-      type: 'WEB3_ACCOUNT_LOADED',
-      account
-    }
+export function web3AccountLoaded(account) {
+  return {
+    type: 'WEB3_ACCOUNT_LOADED',
+    account
   }
+}
 
-  // TOKEN
+// TOKEN
 export function tokenLoaded(contract) {
-    return {
-      type: 'TOKEN_LOADED',
-      contract
-    }
+  return {
+    type: 'TOKEN_LOADED',
+    contract
   }
+}
 
-  // EXCHANGE
+// EXCHANGE
 export function exchangeLoaded(contract) {
-    return {
-      type: 'EXCHANGE_LOADED',
-      contract
-    }
+  return {
+    type: 'EXCHANGE_LOADED',
+    contract
   }
+}
 
-  export function cancelledOrdersLoaded(cancelledOrders) {
-    return {
-      type: 'CANCELLED_ORDERS_LOADED',
-      cancelledOrders
-    }
+export function cancelledOrdersLoaded(cancelledOrders) {
+  return {
+    type: 'CANCELLED_ORDERS_LOADED',
+    cancelledOrders
   }
+}
 
-  export function filledOrdersLoaded(filledOrders) {
-    return {
-      type: 'FILLED_ORDERS_LOADED',
-      filledOrders
-    }
+export function filledOrdersLoaded(filledOrders) {
+  return {
+    type: 'FILLED_ORDERS_LOADED',
+    filledOrders
   }
+}
 
-  export function allOrdersLoaded(allOrders) {
-    return {
-      type: 'ALL_ORDERS_LOADED',
-      allOrders
-    }
-  }  
+export function allOrdersLoaded(allOrders) {
+  return {
+    type: 'ALL_ORDERS_LOADED',
+    allOrders
+  }
+}
+
+// Cancel Order
+export function orderCancelling() {
+  return {
+    type: 'ORDER_CANCELLING'
+  }
+}
+
+export function orderCancelled(order) {
+  return {
+    type: 'ORDER_CANCELLED',
+    order
+  }
+}
