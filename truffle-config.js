2cb8a7e517091149066a4a0d97d11a49490273d2
xTeKc <bcd3v@pm.me>
2021-06-28 19:50:47 -0400

    update config for kovan

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,7 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-
 require('babel-register');
 require('babel-polyfill');
 require("dotenv").config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
 const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
@@ -16,12 +15,14 @@ module.exports = {
     kovan: {
       provider: function() {
         return new HDWalletProvider(
-          privateKeys.split(',') //Array of account private keys
-          //Url to ETH node
+          privateKeys.split(','), //Array of account private keys
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` //Url to ETH node
         )
-      }
-    }
-
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
