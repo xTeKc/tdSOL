483ec86440c1b069567889170099077589af47f7
xTeKc <bcd3v@pm.me>
2021-06-28 11:54:11 -0400

    update state

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,6 +2,10 @@ import { combineReducers } from 'redux';
 
 function web3(state = {}, action) {
     switch (action.type) {
+      case 'WEB3_LOADED':
+        return { ...state,  connection: action.connection }
+      case 'WEB3_ACCOUNT_LOADED':
+        return { ...state, account: action.account }
       default:
         return state
     }
