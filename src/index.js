5ba74bed0c0f4cb6e32c8fb2f2bf876034c900d2
xTeKc <bcd3v@pm.me>
2021-06-28 11:16:25 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider><App /></Provider>
+    <Provider store={store}>
+      <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
