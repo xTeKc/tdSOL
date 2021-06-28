1c20573eb8ec9b7b573c7af9fa1c86059d4e22ba
xTeKc <bcd3v@pm.me>
2021-06-28 11:56:13 -0400

    add token func

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -11,6 +11,15 @@ function web3(state = {}, action) {
     }
   }
 
+  function token(state = {}, action) {
+    switch (action.type) {
+      case 'TOKEN_LOADED':
+        return { ...state, contract: action.contract }
+      default:
+        return state
+    }
+  }
+
 const rootReducer = combineReducers({
     web3: web3
 })
