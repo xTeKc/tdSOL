b9242c4a27b921e4b95d3dcccf90383fff6401a3
xTeKc <bcd3v@pm.me>
2021-06-28 14:20:36 -0400

    import exchangeSelector

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { exchangeSelector } from '../store/selectors'
 import { loadAllOrders } from '../store/interactions'
 
 class Content extends Component {
