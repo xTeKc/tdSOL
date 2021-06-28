5f2b9c784067b4732f1c981ea8e00a922ef99fc4
xTeKc <bcd3v@pm.me>
2021-06-28 16:58:31 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
 import configureStore from './store/configureStore';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore()
 
