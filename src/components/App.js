a6e9d2e5c9a4fd7678c80484d7c4c90e2ac7d4a0
xTeKc <bcd3v@pm.me>
2021-06-28 12:55:16 -0400

    refactor load bc

--- src/components/App.js
+++ src/components/App.js
@@ -20,9 +20,11 @@ class App extends Component {
   async loadBlockchainData(dispatch) {
     const web3 = await loadWeb3(dispatch)
     const networkId = await web3.eth.net.getId()
-    const accounts = await loadAccount(web3, dispatch)
+    await loadAccount(web3, dispatch)
     const token = await loadToken(web3, networkId, dispatch)
-    const exchange = await loadExchange(web3, networkId, dispatch)
+    if(!token) {
+      window.alert('Token smart contract not detected on the current network. Please select another network with Metamask.')
+      return
   }
 
   render() {
