461118e7eab269dc218854befe1a6fc77468f1e6
xTeKc <bcd3v@pm.me>
2021-06-28 11:38:36 -0400

    import connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3'
+import { connect } from 'react-redux'
 import Token from '../abis/Token.json'
 
 class App extends Component {
