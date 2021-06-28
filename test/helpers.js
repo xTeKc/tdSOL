dacb547fd803a37e06795ef1136f91f49acb5ce9
xTeKc <bcd3v@pm.me>
2021-06-28 14:30:41 -0400

    update helpers

--- test/helpers.js
+++ test/helpers.js
@@ -3,6 +3,8 @@ export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const GREEN = 'success'
 export const RED = 'danger'
 
+export const DECIMALS = (10**18)
+
 export const EVM_REVERT = 'VM Exception while processing transaction: revert'
 
 export const ether = (n) => {
