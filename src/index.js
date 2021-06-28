322bb119da3e306c89359d46ea2430d1879872ea
xTeKc <bcd3v@pm.me>
2021-06-28 11:10:05 -0400

    import configureStore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import configureStore from './store/configureStore';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
