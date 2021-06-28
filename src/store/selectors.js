67400366c7defb19d3a0967762b80ea4564d6d67
xTeKc <bcd3v@pm.me>
2021-06-28 14:28:23 -0400

    import moment

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,6 @@
 import { get } from 'lodash'
 import { createSelector } from 'reselect'
+import moment from 'moment'
 
 const account = state => get(state, 'web3.account')
 export const accountSelector = createSelector(account, a => a)
