0de9543fd656bc407f87c7722e2c601476877de0
xTeKc <bcd3v@pm.me>
2021-06-28 11:07:03 -0400

    add middleware and enhancers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from "./reducers";
 
+const loggerMiddleware = createLogger()
+const middleware = []
+
+//For Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export default function configureStore(preloadedState) {
     return createStore(
         rootReducer,
